b9c5463501b2ca595f692566a5a0467fa19cbed1
ze.coa50@gmail.com <ze.coa50@gmail.com>
2025-07-30 12:27:47 +0100

    something

--- main_menu.gd
+++ main_menu.gd
@@ -7,6 +7,7 @@ func _on_start_pressed():
 
 
 func _on_options_pressed() -> void:
+	print("pressed")
 	get_tree().change_scene_to_file("res://Scenes/Options.tscn") # Opens Options
 
 
--- options.gd
+++ options.gd
@@ -1,1 +1,15 @@
 extends Button
+
+func _ready() -> void:
+	var volume20 = get_node("Variables/VolGrid/20")
+	volume20.pressed.connect(
+		func twentyPercent():
+			print("20%")
+			AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(-20))
+			)
+		
+
+#func volumeControl():
+#	if $"Variables/VolGrid/20".is_pressed():
+#		modulate.$Variables/VolGrid/ColorRect5 = 100
+	
