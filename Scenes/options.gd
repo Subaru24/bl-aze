71e390b4e15fe189a2ea1444cf9258ae9590db31
ze.coa50@gmail.com <ze.coa50@gmail.com>
2025-08-01 14:50:46 +0100

    volume fully working (ui is whatever)

--- Scenes/options.gd
+++ Scenes/options.gd
@@ -5,27 +5,54 @@ extends Control
 @onready var eightyBar = $"Variables/VolGrid/80Rect"
 @onready var hundredBar = $"Variables/VolGrid/100Rect"
 @onready var nodeArr = [twentyBar,fourtyBar,sixtyBar,eightyBar,hundredBar]
+#AudioServer.get_bus_index("Master")
+@onready var master = AudioServer.get_bus_index("Master")
+@onready var masterfull = AudioServer.get_bus_index("Master")
 
-#func _ready() -> void:
-	
+
+
+
+func _ready() -> void:
+	AudioServer.set_bus_volume_linear(master,1)
 	
 
 func checkVol(rectArr):
 	var index = 0
 	for rect in rectArr:
 		if rectArr[index] == false:
-			nodeArr[index].modulate =  Color (1,1,1,0.5)
+			nodeArr[index].self_modulate =  Color (1,1,1,0.5)
+		elif rectArr[index] == true:
+			nodeArr[index].self_modulate =  Color (1,1,1,1)
 		index += 1
-
-
-			
-
-	 
+	
 
 
 func _on_20_pressed() -> void:
 	print("20%")
 	var locRectArr = [true,false,false,false,false]
 	checkVol(locRectArr)
-	
-	
+	AudioServer.set_bus_volume_linear(master, 0.2)
+
+func _on_40_pressed() -> void:
+	print("40%")
+	var locRectArr = [true,true,false,false,false]
+	checkVol(locRectArr)
+	AudioServer.set_bus_volume_linear(master, 0.4)
+
+func _on_60_pressed() -> void:
+	print("60%")
+	var locRectArr = [true,true,true,false,false]
+	checkVol(locRectArr)
+	AudioServer.set_bus_volume_linear(master, 0.6)
+
+func _on_80_pressed() -> void:
+	print("80%")
+	var locRectArr = [true,true,true,true,false]
+	checkVol(locRectArr)
+	AudioServer.set_bus_volume_linear(master, 0.8)
+
+func _on_100_pressed() -> void:
+	print("100%")
+	var locRectArr = [true,true,true,true,true]
+	checkVol(locRectArr)
+	AudioServer.set_bus_volume_linear(master, 1)
