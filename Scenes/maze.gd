5af6db65815e1469c083a707239fc9bdced3b941
ze.coa50@gmail.com <ze.coa50@gmail.com>
2025-09-30 21:12:25 +0100

    generation finally works (text)

--- Scenes/maze.gd
+++ Scenes/maze.gd
@@ -11,6 +11,9 @@ const E = 2
 const S = 4
 const W = 8
 
+var rowSize = 20
+var colSize = 20
+
 var cellWalls = {
 
 N: Vector2(0,-1),
@@ -22,24 +25,33 @@ W: Vector2(-1,0)
 
 var cellPos = Vector2(8,0)
 
-var cellWallsText = {
+#var cellWallsText = {
+#
+#N: Vector2(0,-1),
+#E: Vector2(1,0),
+#S: Vector2(0,1),
+#W: Vector2(-1,0)
+#
+#}
 
-N: Vector2(0,-1),
-E: Vector2(1,0),
-S: Vector2(0,1),
-W: Vector2(-1,0)
+#var textWallKeys = cellWallsText.values()
 
-}
+var direction = [
+	[-2,0],
+	[0,2],
+	[2,0],
+	[0,-2]
+]
 
-var textWallKeys = cellWallsText.values()
 
 @onready var visited = []
 
 func _ready() -> void:
-	mazeInit(10,10)
-	# maze[newPos.y][newPos.x] = 10 # x and y are swapped cuz idek
-	generateMaze(cellPos)
+	mazeInit(rowSize,colSize)
+	maze[1][1] = 0
+	generateMaze(1,1)
 	displayMaze()
+
 
 
 # Generate the grid of the Maze
@@ -47,7 +59,7 @@ func mazeInit(rows,cols):
 	for r in range(rows):
 		rows = []
 		for c in range(cols):
-			rows.append(0)
+			rows.append(1)
 		maze.append(rows)
 
 # Shows the maze on screen
@@ -55,26 +67,37 @@ func displayMaze():
 	for row in maze:
 		print(row)
 
-func checkNeighbours(currentCell):
-	var neighbourList = []
-	for val in textWallKeys:
-		var dir = currentCell + val
-		if dir not in visited:
-			neighbourList.append(dir)
-	return neighbourList 
+#func checkNeighbours(currentCell):
+#	var neighbourList = []
+#	for val in textWallKeys:
+#		var dir = currentCell + val
+#		if dir not in visited:
+#			neighbourList.append(dir)
+#	return neighbourList 
 
 	
 	
+
+#func generateMaze(cell):
+#	mazeInit(20,20)
+#	var neighbourList = checkNeighbours(cell)
+#	if neighbourList.size() > 0:
+#		visited.append(cell)
+#		var neighbour = neighbourList.pick_random()
+#		maze[neighbour.x][neighbour.y] = 1
+#		generateMaze(neighbour)
+#	else:
+#		return cell
 
 # Actually create the final maze pattern
-func generateMaze(cell):
-	var neighbourList = checkNeighbours(cell)
-	if neighbourList.size() > 0:
-		visited.append(cell)
-		var neighbour = neighbourList.pick_random()
-		maze[neighbour.x][neighbour.y] = 1
-		generateMaze(neighbour)
-	else:
-		return cell
-
-	
+func generateMaze(currentRow,currentCol):
+	direction.shuffle()
+	for dir in direction:
+		var dirRow = dir[0]
+		var dirCol = dir[1]
+		var newRow = dirRow + currentRow
+		var newCol = dirCol + currentCol
+		if newRow in range(1, rowSize - 1) and newCol in range(1, colSize - 1) and maze[newRow][newRow] == 1:
+			maze[dirRow/2 + currentRow][dirCol/2 + currentCol] = 0
+			maze[newRow][newCol] = 0
+			generateMaze(newRow,newCol)
