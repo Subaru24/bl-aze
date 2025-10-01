835d13c5ef74f55b0ec04919c9e02b1f5a81f64c
Subaru24 <ze.coa50@gmail.com>
2025-10-01 17:29:59 +0100

    works now

--- Scenes/maze.gd
+++ Scenes/maze.gd
@@ -3,31 +3,16 @@ extends Node2D
 @onready var maze = []
 @onready var tilemapLayer = $MazeTile
 
-const WALL = Vector2i(0,0)
-const PATH = Vector2i(1,0)
+const WALL = Vector2i(1,0)
+const PATH = Vector2i(0,0)
 
-const WALLCOLOR = Color("#ffffff")
-const PATHCOLOR = Color("#000000")
 
-const N = 1
-const E = 2
-const S = 4
-const W = 8
 
-var rowSize = 100
-var colSize = 100
-var cellSize = 10
 
-var cellWalls = {
+var rowSize = 20
+var colSize = 20
+var cellSize = 20
 
-N: Vector2(0,-1),
-E: Vector2(1,0),
-S: Vector2(0,1),
-W: Vector2(-1,0)
-
-}
-
-var cellPos = Vector2(8,0)
 
 #var cellWallsText = {
 #
@@ -51,21 +36,21 @@ var direction = [
 @onready var visited = []
 
 func _ready() -> void:
-	mazeInit(rowSize,colSize)
+	mazeInit()
 	maze[1][1] = 0
-	generateMaze(1,1)
+	generateMaze()
 	_draw()
 	displayMaze()
 
 
 
 # Generate the grid of the Maze
-func mazeInit(rows,cols): 
-	for r in range(rows):
-		rows = []
-		for c in range(cols):
-			rows.append(1)
-		maze.append(rows)
+func mazeInit(): 
+	for r in range(rowSize):
+		var row = []
+		for c in range(colSize):
+			row.append(1)
+		maze.append(row)
 
 # Shows the maze on screen
 func displayMaze():
@@ -95,23 +80,32 @@ func displayMaze():
 #		return cell
 
 # Actually create the final maze pattern
-func generateMaze(currentRow,currentCol):
+func generateMaze():
+	mazeInit()
+	var startrow = 1
+	var startcol = 1
+	maze[startrow][startcol] = 0
+	makePath(startrow,startcol)
+
+func makePath(currentRow,currentCol):
 	direction.shuffle()
 	for dir in direction:
 		var dirRow = dir[0]
 		var dirCol = dir[1]
 		var newRow = dirRow + currentRow
 		var newCol = dirCol + currentCol
-		if newRow in range(1, rowSize - 1) and newCol in range(1, colSize - 1) and maze[newRow][newRow] == 1:
-			maze[dirRow/2 + currentRow][dirCol/2 + currentCol] = 0
+		if newRow in range(1, rowSize - 1) and newCol in range(1, colSize - 1) and maze[newRow][newCol] == 1:
+			maze[dirRow / 2 + currentRow][dirCol / 2 + currentCol] = 0
 			maze[newRow][newCol] = 0
-			generateMaze(newRow,newCol)
+			makePath(newRow,newCol)
 			
 
 func _draw():
+	tilemapLayer.clear()
+	
 	# Draw the maze
 	for rows in range(rowSize):
 		for cols in range(colSize):
-			var color = WALLCOLOR if maze[rows][cols] == 1 else PATHCOLOR
-			var rect = Rect2(cols * cellSize, rows * cellSize, cellSize, cellSize)
-			draw_rect(rect, color)
+			var tile = WALL if maze[rows][cols] == 1 else PATH
+			tilemapLayer.set_cell(Vector2i(cols,rows),0,tile)
+			
