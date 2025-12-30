e153ba321ab7dec2f50de35e1c2c69bb36151289
ze.coa50@gmail.com <ze.coa50@gmail.com>
2025-12-30 11:38:48 +0000

    minimap test in other scene

--- Scripts/startgame.gd
+++ Scripts/startgame.gd
@@ -1,5 +1,28 @@
 extends Control
 
-
+const USERPATH = "user://user-options.cfg"
+@onready var map = $Sprite2D
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Maze.tscn")
+	
+
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	
+func _ready() -> void:
+	minimapCheck ()
+	pass
+	
+func minimapCheck ():
+	var options = ConfigFile.new()
+	options.load(USERPATH)
+	var isMinimap = options.get_value("User","Minimap",true)
+	if isMinimap:
+		map.visible = true
+	else: 
+		map.visible = false
+		
+	
+	
